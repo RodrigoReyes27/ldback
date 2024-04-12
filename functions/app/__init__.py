9d0bfc962e04424a8b646fa1df32f03752b51bdf
Rodrigo Reyes <89705213+RodrigoReyes27@users.noreply.github.com>
2024-04-12 07:46:05 -0600

    Login and create accocunt w/ email

--- functions/app/__init__.py
+++ functions/app/__init__.py
@@ -1,5 +1,22 @@
 from flask import Flask
+from os import getenv
+from dotenv import load_dotenv
+from firebase_admin import credentials, initialize_app
 
+load_dotenv()
+
+FIREBASE_CREDENTIALS = credentials.Certificate("app/cert/frida-research-firebase-adminsdk-krmnc-3f0287837c.json")
+
+FIREBASE_CONFIG = {
+    "apiKey": getenv("API_KEY"),
+    "authDomain": getenv("AUTH_DOMAIN"),
+    "databaseURL": getenv("DATABASE_URL"),
+    "projectId": getenv("PROJECT_ID"),
+    "storageBucket": getenv("STORAGE_BUCKET"),
+    "appId": getenv("APP_ID"),
+}
+
+FIREBASE_ADMIN = initialize_app(FIREBASE_CREDENTIALS, FIREBASE_CONFIG)
 
 def create_app() -> Flask:
     app = Flask(__name__)
--- functions/app/user/__init__.py
+++ functions/app/user/__init__.py
@@ -5,3 +5,5 @@ user_blueprint = Blueprint("user_blueprint", __name__)
 # prevent circular imports
 from .create_account import *
 from .delete_account import *
+from .login_email import *
+from .create_account_email import *
