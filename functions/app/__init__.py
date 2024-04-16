7f75773f94ee4299f01534120043e69d556b9a24
Minjares <A01284650@tec.mx>
2024-04-15 22:38:31 -0600

    Se añadio ruta para subir ejercicio

--- functions/app/__init__.py
+++ functions/app/__init__.py
@@ -1,11 +1,15 @@
 from flask import Flask
+from flask_cors import CORS
 
+cors = CORS()
 
 def create_app() -> Flask:
     app = Flask(__name__)
-
+    cors.init_app(app)
     from .user import user_blueprint
-
+    
     app.register_blueprint(user_blueprint, url_prefix="/user")
+    from .document import document_blueprint
+    app.register_blueprint(document_blueprint, url_prefix="/document")
 
     return app
