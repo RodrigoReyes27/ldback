0040c9f83135121f4fdb7411285feb4c1cc82df3
AdrianM0Hdz <adrian.hernandez.p0@gmail.com>
2024-04-16 12:07:46 -0600

    added usage of FIREBASE_CONFIG from infrastructure.firebase

--- functions/app/documentos/parse_pptx.py
+++ functions/app/documentos/parse_pptx.py
@@ -1,5 +1,6 @@
 from flask import jsonify, request
 from pptx import Presentation
+
 # from google.cloud import storage
 from pptx.enum.shapes import MSO_SHAPE_TYPE
 from io import BytesIO
@@ -11,16 +12,17 @@ from firebase_admin import storage
 import os  # Import the os module to work with file paths
 
 from . import documentos_blueprint
-from .. import FIREBASE_CONFIG
+from infrastructure.firebase import FIREBASE_CONFIG
 
 # load_dotenv()
 
+
 @documentos_blueprint.route("/parse_pptx", methods=["POST"])
 def parsear_pptx_handle():
-    bucket_name = FIREBASE_CONFIG['STORAGE_BUCKET']
-    storage_client = storage.Client(project=FIREBASE_CONFIG['PROJECT_ID'])
+    bucket_name = FIREBASE_CONFIG["STORAGE_BUCKET"]
+    storage_client = storage.Client(project=FIREBASE_CONFIG["PROJECT_ID"])
     bucket = storage_client.bucket(bucket_name)
-    blob_name = request.args.get('file_name')
+    blob_name = request.args.get("file_name")
     blob = bucket.blob(blob_name)
     contents = blob.download_as_bytes()
     prs = Presentation(BytesIO(contents))
@@ -29,6 +31,7 @@ def parsear_pptx_handle():
     imglist = extract_images(prs, bucket_name)
 
     return jsonify(msg=text_runs, imglist=imglist)
+
 
 def extract_text(prs):
     text_runs = ""
@@ -41,6 +44,7 @@ def extract_text(prs):
                     text_runs += run.text
                 text_runs += "\n"
     return text_runs
+
 
 def extract_images(prs, bucket_name):
     imglist = []
@@ -57,15 +61,17 @@ def extract_images(prs, bucket_name):
                 n += 1
     return imglist
 
+
 def upload_image(shape, n, bucket_name):
     image = shape.image
     image_bytes = image.blob
-    image_filename = 'jairexampleimage{:03d}.{}'.format(n, image.ext)
-    storage_client = storage.Client(project=FIREBASE_CONFIG['PROJECT_ID'])
+    image_filename = "jairexampleimage{:03d}.{}".format(n, image.ext)
+    storage_client = storage.Client(project=FIREBASE_CONFIG["PROJECT_ID"])
     bucket = storage_client.bucket(bucket_name)
     blob = bucket.blob(image_filename)
     blob.upload_from_string(image_bytes)
     return image_filename
+
 
 # @user_blueprint.route("/parse_pptx", methods=["POST"])
 # def parsear_pptx_handle():
@@ -123,7 +129,7 @@ def upload_image(shape, n, bucket_name):
 #         blob = bucket.blob("{image_filename}")
 
 #         imglist.append(image_filename)
-        
+
 #         with open(image_path, 'wb') as f:
 #             f.write(image_bytes)
 #             blob.upload_from_string(f)
--- functions/app/user/login_email.py
+++ functions/app/user/login_email.py
@@ -3,14 +3,14 @@ import requests
 import json
 
 from . import user_blueprint
-from .. import FIREBASE_CONFIG
+from infrastructure.firebase import FIREBASE_CONFIG
 
 
 @user_blueprint.route("/login_email", methods=["POST"])
 def login_email_handle():
     try:
         data = request.get_json()
-    except :
+    except:
         return jsonify(msg=f"Email and Password must be set"), 400
 
     email: str
@@ -20,13 +20,12 @@ def login_email_handle():
         password = data["password"]
     except KeyError:
         return jsonify(msg=f"Email and Password must be set"), 400
-    
 
-    payload = {
-        "email": email, 
-        "password": password
-    }
-    auth_response = requests.post(f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={FIREBASE_CONFIG['apiKey']}", data=payload)
+    payload = {"email": email, "password": password}
+    auth_response = requests.post(
+        f"https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key={FIREBASE_CONFIG['apiKey']}",
+        data=payload,
+    )
 
     if not auth_response.status_code == 200:
         return jsonify(msg=f"Incorrect Email or Password"), 401
