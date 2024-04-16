1540e5c0a2a36586d0453a163e86753740d60385
AdrianM0Hdz <adrian.hernandez.p0@gmail.com>
2024-04-16 17:55:45 -0600

    parsers structure

---
+++ functions/application/parser/iparser.py
@@ -0,0 +1,28 @@
+from typing import Tuple
+from io import BytesIO
+from abc import ABC, abstractmethod
+
+from pydantic import BaseModel, ConfigDict, Field
+
+
+class DocumentImage(BaseModel):
+    model_config = ConfigDict(frozen=True)
+
+    name: str = Field()
+    data: BytesIO = Field()
+
+
+class ParsingResult(BaseModel):
+    model_config = ConfigDict(frozen=True)
+
+    text: str = Field()
+    images: Tuple[DocumentImage] = Field()
+
+
+class IParser(ABC):
+    """Takes a file determined by the implementation of the interface and
+    returns an object of type Parsing Result
+    """
+
+    @abstractmethod
+    def parse(self, file: BytesIO) -> ParsingResult: ...
